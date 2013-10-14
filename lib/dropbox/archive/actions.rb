5ee52adcc133b421aca7003453ce732b8594dd22
Josh McArthur <joshua.mcarthur@gmail.com>
2013-10-14 16:29:47 +1300

    Implement actions to upload file to dropbox and remove current file

--- lib/dropbox/archive/actions.rb
+++ lib/dropbox/archive/actions.rb
@@ -3,14 +3,14 @@ module Dropbox
     module Actions
 
       def upload(path)
+        file = File.open(path)
+        Dropbox::Archive.client.put_file(File.basename(path), file)
+        remove(path)
       end
 
       def remove(path)
+        FileUtils.rm(path)
       end
-
-      def cancel_upload(path)
-      end
-
     end
   end
 end
