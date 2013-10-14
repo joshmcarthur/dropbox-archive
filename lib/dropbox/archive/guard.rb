145e24e398826963e21897924f157d5850afe743
Josh McArthur <joshua.mcarthur@gmail.com>
2013-10-14 16:29:28 +1300

    Remove guard plugin (dropping down to lower level for directory watching)

--- lib/dropbox/archive/guard.rb
+++
@@ -1,9 +0,0 @@
-module Dropbox
-  module Archive
-    class Guard < Guard::Guard
-      def run_on_changes(paths)
-        paths.each { |path| Dropbox::Archive.upload(path) }
-      end
-    end
-  end
-end
