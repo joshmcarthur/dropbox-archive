8c2796d41b3d6ab4035324310bcc771fbe7cccb3
Josh McArthur <joshua.mcarthur@gmail.com>
2013-10-14 14:01:29 +1300

    Add implementations of all required files

--- lib/dropbox/archive.rb
+++ lib/dropbox/archive.rb
@@ -1,7 +1,17 @@
 require "dropbox/archive/version"
+require "dropbox/archive/actions"
+require "dropbox/archive/configuration"
+require "dropbox/archive/authorization"
+require "dropbox/archive/cli"
+require "dropbox/archive/guard"
+require "dropbox/archive/monitor"
 
 module Dropbox
   module Archive
-    # Your code goes here...
+    extend Actions
+
+    def self.config
+      @config ||= Configuration.new("~/.dropbox_archive")
+    end
   end
 end
