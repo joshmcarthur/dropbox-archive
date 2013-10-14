58e34c4045cb56d73012dc6d33e9af769a4b66e2
Josh McArthur <joshua.mcarthur@gmail.com>
2013-10-14 16:28:15 +1300

    Implement client accessor on main module

--- lib/dropbox/archive.rb
+++ lib/dropbox/archive.rb
@@ -3,8 +3,8 @@ require "dropbox/archive/actions"
 require "dropbox/archive/configuration"
 require "dropbox/archive/authorization"
 require "dropbox/archive/cli"
-require "dropbox/archive/guard"
 require "dropbox/archive/monitor"
+require "dropbox_sdk"
 
 module Dropbox
   module Archive
@@ -12,6 +12,14 @@ module Dropbox
 
     def self.config
       @config ||= Configuration.new("~/.dropbox_archive")
+    end
+
+    def self.client
+      if Dropbox::Archive.config.get('dropbox_access_token').nil?
+        raise "No access token configured! Please run dropbox-archive setup."
+      end
+
+      @client ||= ::DropboxClient.new(Dropbox::Archive.config.get('dropbox_access_token'))
     end
   end
 end
